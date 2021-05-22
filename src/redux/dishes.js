7e4c9f53cf73e4faae8f1bfa2d5307d1c122a3af
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-05-22 18:16:39 +0530

    redux thunk and logger exercise

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,18 @@
-import {DISHES} from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
+
+export const Dishes = (state = {
+        isLoading: true,
+        errMess: null,
+        dishes: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return{ ...state, isLoading:false, errMess: null, dishes:action.payload}
+        case ActionTypes.DISHES_LOADING:
+            return{ ...state, isLoading:true, errMess: null, dishes:[]}//new object state is created, and changes are done in new one, /(changes are the one after 1st parameter)
+        case ActionTypes.DISHES_FAILED:
+            return{ ...state, isLoading:false, errMess: action.payload, dishes:[]}
         default:
             return state;
     }
