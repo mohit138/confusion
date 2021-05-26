87a9a6be80b2db70e4816f8faa5a65e4c262e629
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-05-27 04:29:00 +0530

    react animation components exercise

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const maxLength = (len) => (val) => !(val) || (val.length<=len);
 const minLength = (len) => (val) => (val) && (val.length>=len);
@@ -97,19 +98,23 @@ const minLength = (len) => (val) => (val) && (val.length>=len);
         
             const disp = comments.map((com)=>{
                 return(
-                    
-                    <div key={com.id} className=" list-unstyled">
-                        <div className="mt-3">{com.comment}</div>
-                        <div className="mt-3">--{com.author},{" "}{new Intl.DateTimeFormat('en-US', {year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(com.date)))}</div>
-                    </div>
-                    
+                    <Fade in>
+                        <li key={com.id}>
+                            <div className="mt-3">{com.comment}</div>
+                            <div className="mt-3">--{com.author},{" "}{new Intl.DateTimeFormat('en-US', {year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(com.date)))}</div>
+                        </li>
+                    </Fade>                    
                 );
             });
 
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    {disp}
+                    <ul  className=" list-unstyled">
+                        <Stagger in>
+                            {disp}
+                        </Stagger>
+                    </ul>
                     <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
@@ -119,13 +124,18 @@ const minLength = (len) => (val) => (val) && (val.length>=len);
         if(dish!=null) {
             return(
                 <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name}/>
-                        <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in 
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name}/>
+                            <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>              
             );
         }
